0e5e7fbc20ac7900a28936f18bbf997a3e3de90a
DarkDirectX <kirill.salti@gmail.com>
2019-03-27 00:16:28 +0500

    Initial Commit

---
+++ GistsViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GistsViewer
+//
+//  Created by Кирилл Салтыков on 27/03/2019.
+//  Copyright © 2019 Kirill Saltykov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
